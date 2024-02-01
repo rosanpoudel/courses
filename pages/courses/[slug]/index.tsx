433fb26cd00b7b208c2b49f156847d2be85e7c3b
roshan12iD <roshan@12id.com>
2024-02-01 07:52:24 +0545

    button added

--- pages/courses/[slug]/index.tsx
+++ pages/courses/[slug]/index.tsx
@@ -38,52 +38,60 @@ const CourseDetails = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="container my-12 mx-auto">
-        <div className="grid w-full gap-8 sm:grid-cols-1  mb-8">
-          <div>
+        {pageDetails?.title ? (
+          <div className="grid w-full gap-8 sm:grid-cols-1  mb-8">
             <div>
-              <img
-                className="w-full object-cover rounded-t-lg h-96"
-                src={pageDetails?.coverImage}
-                alt={pageDetails?.title}
-              />
-            </div>
-          </div>
-          <div className="pax-5">
-            <div className="mb-5">
               <div>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-stone-600">
-                  {pageDetails?.title}
-                </h5>
-                <p className="mb-5 font-normal text-xl text-gray-600">
-                  {removeHtmlTags(pageDetails?.description)}
-                </p>
-
-                <div>
-                  <p className="font-medium text-lg text-gray-800">
-                    <span className="font-bold">Author:</span>{" "}
-                    {pageDetails?.instructorName}
-                  </p>
-
-                  <p className="font-medium text-lg text-gray-800">
-                    <span className="font-bold">Views:</span>{" "}
-                    {pageDetails?.viewsCount}
-                  </p>
-
-                  <p className="font-medium text-lg text-gray-800">
-                    <span className="font-bold">Created At:</span>{" "}
-                    {pageDetails?.creationDate}
-                  </p>
-                </div>
+                <img
+                  className="w-full object-cover rounded-t-lg h-96"
+                  src={pageDetails?.coverImage}
+                  alt={pageDetails?.title}
+                />
               </div>
             </div>
+            <div className="px-5">
+              <div className="mb-5">
+                <div>
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-stone-600">
+                    {pageDetails?.title}
+                  </h5>
+                  <p className="mb-5 font-normal text-xl text-gray-600">
+                    {removeHtmlTags(pageDetails?.description)}
+                  </p>
 
-            <Link className="" href="/courses">
-              <button className="w-48 bg-blue-500 text-white text-lg  py-1 px-4 rounded">
-                Back to courses
-              </button>
-            </Link>
+                  <div>
+                    <p className="font-medium text-lg text-gray-800">
+                      <span className="font-bold">Author:</span>{" "}
+                      {pageDetails?.instructorName}
+                    </p>
+
+                    <p className="font-medium text-lg text-gray-800">
+                      <span className="font-bold">Views:</span>{" "}
+                      {pageDetails?.viewsCount}
+                    </p>
+
+                    <p className="font-medium text-lg text-gray-800">
+                      <span className="font-bold">Created At:</span>{" "}
+                      {pageDetails?.creationDate}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <Link className="" href="/courses">
+                <button className="w-48 bg-blue-500 text-white text-lg  py-1 px-4 rounded">
+                  Back to courses
+                </button>
+              </Link>
+            </div>
           </div>
-        </div>
+        ) : (
+          <Link className="" href="/courses">
+            <button className="w-48 bg-blue-500 text-white text-lg  py-1 px-4 rounded">
+              Back to courses
+            </button>
+          </Link>
+        )}
       </main>
     </div>
   );
